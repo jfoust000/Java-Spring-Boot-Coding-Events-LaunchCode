c0dff071a23bc955f2d9737681ab19aba0f730c5
Joel Foust <joel.foust@yahoo.com>
2023-09-15 15:25:48 -0400

    Implemented Model Binding

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,10 +1,29 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
 
+    private int id;
+    private static int nextId = 1;
     private String name;
-    public Event(String name) {
+    private String description;
+
+    public Event() {
+
+        this.id = nextId;
+        nextId++;
+
+    }
+
+    public Event(String name, String description) {
+        this();
         this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -15,9 +34,30 @@ public class Event {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
 }
